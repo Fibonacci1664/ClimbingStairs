3a0374129ef82683928b17d4707b7199bfe6b49a
Fibonacci <dave.green1664@yahoo.co.uk>
2021-09-02 11:03:46 +0100

    Done! super easy compared to recursion AND with it only being a single loop on 'n' elements the time complexity is O(n) instead of O(2^n) with recursion. I'm really not seeing the benefits of recursion at all. It's more complex to understand, and debug, AND takes longer to execute. Why is recursion even a thing? spoken like  a true novice I'm sure.

--- ClimbingStairs/Main.cpp
+++ ClimbingStairs/Main.cpp
@@ -19,7 +19,7 @@ void createFibSequence()
     fibSeq.push_back(0);
     fibSeq.push_back(1);
 
-    for (int i = 2; i < 45; ++i)
+    for (int i = 2; i < 47; ++i)
     {
         fibSeq.push_back(fibSeq[i - 1] + fibSeq[i - 2]);
     }
@@ -27,28 +27,39 @@ void createFibSequence()
 
 int climbStairs(int n)
 {
-    return fib(n + 1);
+    return fibSeq[n + 1];
 }
 
 int main()
 {
     // TEST CASES
-    /*int test1 = 2;
+    int test1 = 2;
     int test2 = 3;
     int test3 = 5;
     int test4 = 7;
     int test5 = 16;
     int test6 = 4;
-    int test7 = 42;
+    int test7 = 44;
 
     int ans = 0;
 
-    for (int i = 0; i < 45; ++i)
+    // This was used to recognise that it wasn't that the code was wrong it was just that recursion was taking soooooooooooooooooo long.
+    /*for (int i = 0; i < 45; ++i)
     {
         ans = climbStairs(i);
 
         std::cout << "With " << i << " number of stairs, there are " << ans << " possible ways to climb them.\n";
     }*/
+
+    createFibSequence();
+    ans = climbStairs(test7);
+
+    std::cout << "With " << test7 << " number of stairs, there are " << ans << " possible ways to climb them.\n";
+
+    for (int i = 0; i < fibSeq.size(); ++i)
+    {
+        std::cout << i << " Stairs " << fibSeq[i] << '\n';
+    }
 
 	return 0;
 }
